680bdcd914a9ea8b1cb23a053c99ae8ae7fae442
tswaby <tswaby@uwyo.edu>
2026-08-17 16:17:12 -0600

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  
-  // Set the base path to relative so assets resolve correctly in subdirectories
-  base: './', 
-  
   build: {
-    // Redirect the production build directory from the default 'dist' to 'docs'
-    outDir: 'docs',
-    
-    // Empty the output directory on build to prevent old, unused files from sticking around
+    outDir: 'dist',
     emptyOutDir: true,
   }
 })
